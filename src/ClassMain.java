ed955a441aff5583b0de40d12ae555c875833b45
ansuote <ansuote2011@gmail.com>
2024-03-08 12:58:11 +0800

    feat: 增加选择排序

--- src/ClassMain.java
+++ src/ClassMain.java
@@ -3,18 +3,19 @@
  * @Author: ansuote
  * @Date: 2024-02-21 18:25:05
  * @LastEditors: ansuote
- * @LastEditTime: 2024-03-08 11:22:27
+ * @LastEditTime: 2024-03-08 12:49:22
  */
 
 import sort.BubbleSort;
 import sort.InsertSort;
+import sort.SelectionSort;
 import utils.SortUtils;
 
 public class ClassMain {
     public static void main(String[] args) {
         int[] nums = new int[] { 1, 24, 5, 3 };
         // SortUtils.sort(nums, BubbleSort.class);
-        SortUtils.sort(nums, InsertSort.class);
-
+        // SortUtils.sort(nums, InsertSort.class);
+        SortUtils.sort(nums, SelectionSort.class);
     }
 }
---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,39 @@
+/*
+ * @Descripttion: 选择排序
+ * @Author: ansuote
+ * @Date: 2024-03-08 12:46:52
+ * @LastEditors: ansuote
+ * @LastEditTime: 2024-03-08 12:57:24
+ */
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort implements ISort {
+
+    @Override
+    public int[] sort(int[] originNum) {
+        if (null == originNum || originNum.length == 0) {
+            return originNum;
+        }
+
+        int n = originNum.length;
+        int[] resultNum = Arrays.copyOf(originNum, n);
+
+        for (int i = 0; i < n - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (resultNum[j] < resultNum[index]) {
+                    index = j;
+                }
+            }
+
+            int temp = resultNum[i];
+            resultNum[i] = resultNum[index];
+            resultNum[index] = temp;
+        }
+
+        return resultNum;
+    }
+
+}
