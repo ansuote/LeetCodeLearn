17536e3b4c2d7b8aae4c55d2bcf228aa0d5c633f
ansuote <ansuote2011@gmail.com>
2024-03-08 09:27:48 +0800

    feat: 完善冒泡排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -3,28 +3,41 @@
  * @Author: ansuote
  * @Date: 2024-03-07 19:08:46
  * @LastEditors: ansuote
- * @LastEditTime: 2024-03-07 19:30:14
+ * @LastEditTime: 2024-03-08 09:22:28
  */
 package sort;
 
 import utils.StringUtils;
+import java.util.Arrays;
 
 public class BubbleSort {
-    public void bubbleSort(int[] nums) {
-        if (null == nums || nums.length == 0) {
-            return;
+    public int[] bubbleSort(int[] originNum) {
+        if (null == originNum || originNum.length == 0) {
+            return originNum;
         }
 
-        int n = nums.length;
+        int n = originNum.length;
+
+        int[] nums = Arrays.copyOf(originNum, n);
+
         for (int i = 0; i <= n - 1; i++) {
+            boolean flag = true;
+
             for (int j = 0; j < n - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
+                    flag = false;
                 }
             }
+
+            if (flag) {
+                break;
+            }
         }
+
+        return nums;
     }
 
     public static void test(int[] nums) {
@@ -32,10 +45,10 @@ public class BubbleSort {
         StringUtils.println(nums);
 
         BubbleSort sort = new BubbleSort();
-        sort.bubbleSort(nums);
+        int[] resultNums = sort.bubbleSort(nums);
 
         System.out.println("执行冒泡排序后");
-        StringUtils.println(nums);
+        StringUtils.println(resultNums);
 
     }
 }
