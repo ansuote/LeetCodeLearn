f6c6d20a5ef3c0a1c3e1823c1e9f629cb256f47f
ansuote <ansuote2011@gmail.com>
2024-03-13 19:41:04 +0800

    feat: 82-删除排序链表中的重复元素

---
+++ src/recursive/Class82.java
@@ -0,0 +1,31 @@
+/*
+ * @Descripttion: 删除排序链表中的重复元素 II
+ * @Author: ansuote
+ * @Date: 2024-03-13 19:30:10
+ * @LastEditors: ansuote
+ * @LastEditTime: 2024-03-13 19:39:22
+ */
+package recursive;
+
+import base.ListNode;
+
+public class Class82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (null == head || null == head.next) {
+            return head;
+        }
+
+        if (head.val != head.next.val) {
+            ListNode node = deleteDuplicates(head.next);
+            head.next = node;
+            return head;
+        } else {
+            ListNode newListNode = head.next;
+            while (null != newListNode && newListNode.val == head.val) {
+                newListNode = newListNode.next;
+            }
+
+            return deleteDuplicates(newListNode);
+        }
+    }
+}
