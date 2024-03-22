d269728fb028748ac09aa0a004893f6c8b059d68
ansuote <ansuote2011@gmail.com>
2024-03-22 20:22:30 +0800

    feat: 69-x的平方根

---
+++ src/pointer/Class69.java
@@ -0,0 +1,31 @@
+/*
+ * @Descripttion: x的平方根
+ * @Author: ansuote
+ * @Date: 2024-03-22 19:53:30
+ * @LastEditors: ansuote
+ * @LastEditTime: 2024-03-22 20:22:05
+ */
+package pointer;
+
+public class Class69 {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // 等于x的时候也要保存值
+            if ((long) mid * mid <= x) {
+                left = mid + 1;
+                result = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
