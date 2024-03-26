6c37d91d89e6e8b89cef454ba6168d0f6c686bfd
ansuote <ansuote2011@gmail.com>
2024-03-26 13:49:58 +0800

    feat: 94-二叉树中序遍历-递归版本完善

--- src/binaryTree/Class94.java
+++ src/binaryTree/Class94.java
@@ -3,7 +3,7 @@
  * @Author: ansuote
  * @Date: 2024-02-16 20:24:58
  * @LastEditors: ansuote
- * @LastEditTime: 2024-03-26 10:21:31
+ * @LastEditTime: 2024-03-26 10:54:46
  */
 package binaryTree;
 
@@ -13,27 +13,23 @@ import java.util.List;
 import base.TreeNode;
 
 public class Class94 {
-    private List<Integer> result = new ArrayList<>();
 
-    public static void main(String[] args) {
-
-    }
-
-    public List<Integer> inorderTraversal(TreeNode treeNode) {
-        if (null == treeNode) {
-            return result;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (null == root) {
+            return list;
         }
-        dfs(treeNode);
-        return result;
+        dfs(root, list);
+        return list;
     }
 
-    public void dfs(TreeNode treeNode) {
-        if (null == treeNode) {
+    public void dfs(TreeNode node, List<Integer> list) {
+        if (null == node) {
             return;
         }
 
-        dfs(treeNode.left);
-        result.add(treeNode.val);
-        dfs(treeNode.right);
+        dfs(node.left, list);
+        list.add(node.val);
+        dfs(node.right, list);
     }
 }
